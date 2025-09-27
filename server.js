39ea6f8a7d675d11e702b7b8810b0e20086a6f45
pukechamp <extremexis1@outlook.com>
2025-09-27 00:04:24 -0700

    First Commit

--- server.js
+++ server.js
@@ -2,11 +2,39 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// temp array to act as database
-let users = [];
+// pseudo database for listed sessions
+let sessions_db = [];
 
-// Put your endpoints here
+//Get endpoint to retrieve all sessions
+app.get('/api/sessions', (req, res) => 
+    {
+        res.json(sessions_db);
+    });
 
-// Start the server
+// POST endpoint: Create a new user
+app.post('/api/sessions', (req, res) => {
+  const { host, name, datetime, location } = req.body;
+
+  // Basic validation
+  if (!host || !name || !datetime || !location) {
+    return res.status(400).json({ error: 'You need a name, date, time and location to post your session!' });
+  }
+
+  // Create a new session
+  const newSesh = 
+  {
+    id: sessions_db.length + 1, // simple auto-increment ID
+    host,
+    name,
+    datetime,
+    location
+  };
+
+  // Save to our "database"
+  sessions_db.push(newSesh);
+
+  // Respond with 201 Created and the new session
+  res.status(201).json(newSesh);
+});
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
