38bf5a9c6fb8e1ccc9a4ba2a4859416b45f12eda
brv5079 <brv5079@psu.edu>
2025-09-28 13:25:05 -0400

    GET and PATCH endpoints for users added to server.js

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ app.use(express.json());
 // temp array to act as database
 let users = [];
 
-// Put your endpoints here
+
+// GET endpoint: get all currrent users
+app.get('/api/users', (req, res) => 
+    {
+        res.json(users);
+    });
+
+// PATCH endpoint: update a user's info
+app.patch('/api/users/:email', (req, res) => {
+  const { email } = req.params;
+  const { name, dob } = req.body;
+
+  //Locate the correct user
+  const user = users.find(u => u.email === email);
+  if (!user) {
+    return res.status(404).json({ error: 'User not found' });
+  }
+
+  //Update the fields requested
+  if (name) user.name = name;
+  if (dob) user.dob = dob;
+
+  res.json(user);
+});
 
 // Start the server
 const PORT = 3000;
