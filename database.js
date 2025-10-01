7a69258946532e5ae514a6d905f0d158a7919a73
Miguel Quemado <mquemado16@gmail.com>
2025-10-01 14:00:18 -0400

    Refactored database.js, added close function

--- database.js
+++ database.js
@@ -1,19 +1,35 @@
-require('dotenv').config();
+// Imports
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
 const mongoose = require('mongoose');
 
+// Configure .env database URI
+dotenvExpand.expand(dotenv.config());
 const dbURI = process.env.MONGO_URI;
 
+// Open connection
 const connectDB = async () => {
     try {
         await mongoose.connect(dbURI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            // currently deprecated: useNewUrlParser: true,
+            // currently deprecated: useUnifiedTopology: true,
         });
         console.log('MongoDB connected successfully!');
     }
     catch (err) {
+        console.error('MongoDB connection error:', err.message);
         process.exit(1);
     }
 };
 
+const closeDB = async () => {
+    try {
+        await mongoose.disconnect();
+        console.log('MongoDB connection closed!');
+    } catch (err) {
+        console.error('Error while closing MongoDB connection:', err.message);
+    }
+};
+
 module.exports = connectDB;
+module.exports = closeDB;
