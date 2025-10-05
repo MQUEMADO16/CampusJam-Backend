598a68a0f533d531c2505e30091639f92363fe15
Jacob Norgard <jkn5395@psu.edu>
2025-10-05 15:34:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const app = require('./src/app');
 
+app.get('/api/users', (req, res) => {
+    res.status(200).json(users);
+});
+
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
