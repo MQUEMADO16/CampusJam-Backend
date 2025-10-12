660e66adc4f4625972d9bfe812f8a973e68dffa4
Miguel Quemado <mquemado16@gmail.com>
2025-10-12 18:01:26 -0400

    refactored session route

--- src/routes/session.routes.js
+++ src/routes/session.routes.js
@@ -4,14 +4,14 @@ const sessionController = require('../controllers/session.controller');
 
 // Endpoints
 // TODO: Route all endpoints
-router.get('/sessions', sessionController.getAllSessions);
 
+// Sessions
+router.get('/sessions', sessionController.getAllSessions);
 router.post('/sessions', sessionController.createSession);
 
+// Session by id
 router.get('/sessions/:id', sessionController.getSessionById);
-
 router.put('/sessions/:id', sessionController.updateSession);
-
 router.delete('/sessions/:id', sessionController.deleteSessionById);
 
 module.exports = router;
